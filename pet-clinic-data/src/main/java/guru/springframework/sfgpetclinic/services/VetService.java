ae3a0fbf3567d8caf17b3608529c970a5240f8af
Casgrain <raycasgrain@gmail.com>
2022-08-30 09:14:21 -0400

    base entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.Vet;
 
+import java.util.Set;
 
-/**
- * Created by jt on 7/18/18.
- */
-public interface VetService extends CrudService<Vet, Long>{
+public interface VetService {
 
+    Vet findById(Long id);
+
+    Vet save(Pet pet);
+
+    Set<Vet> findAll();
 }
